570484e3878e7ee5c2af9161d938a03b019d67c3
Kyrylo Maksymenko <saklar13@gmail.com>
2020-12-16 12:42:50 +0200

    update pre-commit, use new version of isort, use pytest for tests

--- tests/cloudshell/test_package.py
+++ tests/cloudshell/test_package.py
@@ -1,8 +1,5 @@
-import unittest
-
 from cloudshell.template.package import info
 
 
-class TestPackage(unittest.TestCase):
-    def test_package(self):
-        self.assertEqual("info", info)
+def test_info():
+    assert info == "info"
