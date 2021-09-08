9ca81ddf6c6e842b9b55d44bb9ebeb516dbe4a6c
Kyrylo Maksymenko <saklar13@gmail.com>
2021-09-08 13:25:08 +0300

    remove py2 support

--- setup.py
+++ setup.py
@@ -1,30 +1,10 @@
-import os
-
 from setuptools import find_packages, setup
-from setuptools.version import __version__ as setuptools_version
-
-if tuple(map(int, setuptools_version.split(".")[:2])) < (40, 0):
-    import sys
-
-    python = sys.executable
-    try:
-        s = os.system('{} -m pip install "setuptools>=40"'.format(python))
-        if s != 0:
-            raise Exception
-    except Exception:
-        raise Exception("Setuptools>40 have to be installed")
-
-    os.execl(python, python, *sys.argv)
 
 
-with open(os.path.join("version.txt")) as version_file:
-    version_from_file = version_file.read().strip()
+def read_file(file_name: str) -> str:
+    with open(file_name) as fo:
+        return fo.read().strip()
 
-with open("requirements.txt") as f_required:
-    required = f_required.read().splitlines()
-
-with open("test_requirements.txt") as f_tests:
-    required_for_tests = f_tests.read().splitlines()
 
 setup(
     name="cloudshell-template",
@@ -32,12 +12,10 @@ setup(
     author="QualiSystems",
     author_email="info@qualisystems.com",
     packages=find_packages(),
-    install_requires=required,
-    tests_require=required_for_tests,
-    python_requires=(
-        ">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*, <4"
-    ),
-    version=version_from_file,
+    install_requires=read_file("requirements.txt"),
+    tests_require=read_file("test_requirements.txt"),
+    python_requires="~=3.7",
+    version=read_file("version.txt"),
     package_data={"": ["*.txt"]},
     description="<your package description>",
     include_package_data=True,
